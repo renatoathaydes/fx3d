299a8604220f6734ce7b6d967e55bbb940012aa7
renato <renato@twobo.com>
2017-04-17 16:19:31 +0200

    Refactored FX3DViewer.

--- src/main/java/com/athaydes/fx3d/FX3DViewer.java
+++ src/main/java/com/athaydes/fx3d/FX3DViewer.java
@@ -1,10 +1,11 @@
 package com.athaydes.fx3d;
 
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
-import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.SubScene;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.input.ZoomEvent;
 import javafx.scene.paint.Color;
@@ -14,9 +15,30 @@ import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 
 /**
- *
+ * A 3D Viewer for JavaFX.
+ * <p>
+ * The content of the viewer can be set with {@link #setContent(Node)}.
+ * <p>
+ * The Viewer itself is represented by a {@link SubScene} which can be obtained with
+ * {@link #getScene()}.
  */
-public class FX3DViewer extends Group {
+public class FX3DViewer {
+
+    private static final double CONTROL_MULTIPLIER = 0.1;
+    private static final double SHIFT_MULTIPLIER = 10.0;
+    private static final double MOUSE_SPEED = 0.1;
+    private static final double ROTATION_SPEED = 2.0;
+    private static final double TRACK_SPEED = 0.3;
+
+    private static final double CAMERA_INITIAL_DISTANCE = -450;
+    private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
+    private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
+    private static final double CAMERA_NEAR_CLIP = 0.1;
+    private static final double CAMERA_FAR_CLIP = 10000.0;
+
+    private final SubScene scene;
+    private final Group root = new Group();
+    private Node content;
 
     private final PerspectiveCamera camera = new PerspectiveCamera( true );
     private final Xform cameraXform = new Xform();
@@ -25,12 +47,6 @@ public class FX3DViewer extends Group {
 
     private final Xform axisGroup = new Xform();
 
-    private static final double CONTROL_MULTIPLIER = 0.1;
-    private static final double SHIFT_MULTIPLIER = 10.0;
-    private static final double MOUSE_SPEED = 0.1;
-    private static final double ROTATION_SPEED = 2.0;
-    private static final double TRACK_SPEED = 0.3;
-
     private double mousePosX;
     private double mousePosY;
     private double mouseOldX;
@@ -38,55 +54,73 @@ public class FX3DViewer extends Group {
     private double mouseDeltaX;
     private double mouseDeltaY;
 
-    private static final double CAMERA_INITIAL_DISTANCE = -450;
-    private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
-    private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
-    private static final double CAMERA_NEAR_CLIP = 0.1;
-    private static final double CAMERA_FAR_CLIP = 10000.0;
-
     private final Translate cameraPosition = new Translate( 0, 0, 0 );
-
-    private final EventHandler<ZoomEvent> zoomEventHandler = event -> {
-        if ( !Double.isNaN( event.getZoomFactor() ) && event.getZoomFactor() > 0.8 && event.getZoomFactor() < 1.2 ) {
-            double z = cameraPosition.getZ() / event.getZoomFactor();
-            z = Math.max( z, -1000 );
-            z = Math.min( z, 0 );
-            cameraPosition.setZ( z );
-        }
-    };
-
-    private final EventHandler<ScrollEvent> scrollEventHandler = event -> {
-        if ( event.getTouchCount() > 0 ) { // touch pad scroll
-            cameraXform2.t.setX( cameraXform2.t.getX() - ( 0.01 * event.getDeltaX() ) );
-            cameraXform2.t.setY( cameraXform2.t.getY() + ( 0.01 * event.getDeltaY() ) );
-        } else {
-            double z = cameraPosition.getZ() - ( event.getDeltaY() * 0.2 );
-            z = Math.max( z, -1000 );
-            z = Math.min( z, 0 );
-            cameraPosition.setZ( z );
-        }
-    };
 
     private SimpleBooleanProperty showAxis = new SimpleBooleanProperty( false ) {
         @Override
         protected void invalidated() {
             if ( get() ) {
-                getChildren().add( axisGroup );
+                root.getChildren().add( axisGroup );
             } else {
-                getChildren().remove( axisGroup );
+                root.getChildren().remove( axisGroup );
             }
         }
     };
 
-    public FX3DViewer( Scene scene ) {
+    public FX3DViewer( double width, double height ) {
+        scene = new SubScene( root, width, height );
+        init();
+    }
+
+    public FX3DViewer( double width, double height, boolean depthBuffer,
+                       SceneAntialiasing antiAliasing ) {
+        scene = new SubScene( root, width, height, depthBuffer, antiAliasing );
+        init();
+    }
+
+    private void init() {
         buildCamera();
         createAxes();
-        handleKeyboard( scene );
-        handleMouse( scene );
+        handleKeyboard();
+        handleMouse();
+        handleScroll();
+        handleZoom();
         scene.setCamera( camera );
-        scene.addEventHandler( ZoomEvent.ANY, zoomEventHandler );
-        scene.addEventHandler( ScrollEvent.ANY, scrollEventHandler );
+        scene.setFill( Color.WHITE );
         showAxis.set( true );
+    }
+
+    /**
+     * Returns the {@link SubScene} of this 3d viewer.
+     * <p>
+     * This is the Node representing this viewer and should be added to the main application
+     * for it to be visible.
+     *
+     * @return the 3D viewer's SubScene
+     */
+    public SubScene getScene() {
+        return scene;
+    }
+
+    /**
+     * @return the content Node.
+     */
+    public Node getContent() {
+        return content;
+    }
+
+    /**
+     * Set the 3D content of this viewer.
+     *
+     * @param content content Node
+     */
+    public void setContent( Node content ) {
+        if ( this.content != null ) {
+            root.getChildren().remove( this.content );
+        }
+
+        this.content = content;
+        root.getChildren().add( content );
     }
 
     public boolean isShowAxis() {
@@ -97,8 +131,12 @@ public class FX3DViewer extends Group {
         return showAxis;
     }
 
+    public Translate getCameraPosition() {
+        return cameraPosition;
+    }
+
     private void buildCamera() {
-        getChildren().add( cameraXform );
+        root.getChildren().add( cameraXform );
         cameraXform.getChildren().add( cameraXform2 );
         cameraXform2.getChildren().add( cameraXform3 );
         cameraXform3.getChildren().add( camera );
@@ -114,10 +152,10 @@ public class FX3DViewer extends Group {
     }
 
     private void createAxes() {
-        axisGroup.getChildren().add( new Axis() );
+        axisGroup.getChildren().add( new AxisGroup() );
     }
 
-    private void handleMouse( Scene scene ) {
+    private void handleMouse() {
         scene.setOnMousePressed( me -> {
             mousePosX = me.getSceneX();
             mousePosY = me.getSceneY();
@@ -159,7 +197,7 @@ public class FX3DViewer extends Group {
         } );
     }
 
-    private void handleKeyboard( Scene scene ) {
+    private void handleKeyboard() {
         scene.setOnKeyPressed( event -> {
             switch ( event.getCode() ) {
                 case Z:
@@ -175,11 +213,36 @@ public class FX3DViewer extends Group {
         } );
     }
 
-    private static class Axis extends Group {
+    private void handleScroll() {
+        scene.addEventHandler( ScrollEvent.ANY, event -> {
+            if ( event.getTouchCount() > 0 ) { // touch pad scroll
+                cameraXform2.t.setX( cameraXform2.t.getX() - ( 0.01 * event.getDeltaX() ) );
+                cameraXform2.t.setY( cameraXform2.t.getY() + ( 0.01 * event.getDeltaY() ) );
+            } else {
+                double z = cameraPosition.getZ() - ( event.getDeltaY() * 0.2 );
+                z = Math.max( z, -1000 );
+                z = Math.min( z, 0 );
+                cameraPosition.setZ( z );
+            }
+        } );
+    }
+
+    private void handleZoom() {
+        scene.addEventHandler( ZoomEvent.ANY, event -> {
+            if ( !Double.isNaN( event.getZoomFactor() ) && event.getZoomFactor() > 0.8 && event.getZoomFactor() < 1.2 ) {
+                double z = cameraPosition.getZ() / event.getZoomFactor();
+                z = Math.max( z, -1000 );
+                z = Math.min( z, 0 );
+                cameraPosition.setZ( z );
+            }
+        } );
+    }
+
+    private static class AxisGroup extends Group {
         private static final double RADIUS = 1.0;
         private static final int LENGTH = 1_000;
 
-        Axis() {
+        AxisGroup() {
             Cylinder axisX = new Cylinder( RADIUS, LENGTH );
             axisX.getTransforms().addAll( new Rotate( 90, Rotate.Z_AXIS ), new Translate( 0, 30, 0 ) );
             axisX.setMaterial( new PhongMaterial( Color.RED ) );
